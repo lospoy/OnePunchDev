ae57219296b4eafaa9cd2c8cf2f4ad6dbe792d06
Carlos <36555654+lospoy@users.noreply.github.com>
2023-02-21 17:33:51 -0500

    trying out struct

--- server/models/sessions.go
+++ server/models/sessions.go
@@ -2,11 +2,21 @@ package models
 
 import "gorm.io/gorm"
 
+type Timespan struct {
+	Start			*int64		`json:"start"`
+	End				*int64		`json:"end"`
+	Duration	*int64		`json:"duration"`
+}
+
+type Code struct {
+	Time			Timespan	`json:"timespan"`
+}
+
 type Sessions struct {
 	ID				uint			`gorm:"primary key;autoIncrement" json:"id"`
-	Code			*string		`json:"code"`
-	Workout		*string		`json:"workout"`
-	Rest			*string		`json:"rest"`
+	Code			Code			`json:"code"`
+	// Workout		*string		`json:"workout"`
+	// Rest			*string		`json:"rest"`
 }
 
 func MigrateSessions(db *gorm.DB) error {
