48e581d085bb4a5050ec0981f0260eadf4043912
Carlos <36555654+lospoy@users.noreply.github.com>
2023-03-06 01:37:06 -0500

    update structs

--- server/models/sessions.go
+++ server/models/sessions.go
@@ -3,23 +3,27 @@ package models
 import "gorm.io/gorm"
 
 type Timespan struct {
-	Start			*int64		`json:"start"`
-	End				*int64		`json:"end"`
-	Duration	*int64		`json:"duration"`
+	Start					*int64		`json:"start"`
+	End						*int64		`json:"end"`
+	CodeID				int
+	Code					Code
 }
 
 type Code struct {
-	Time			Timespan	`json:"timespan"`
+	ID						int
+	SessionsID		int
+	Sessions			Sessions
+	Times					[]Timespan	`json:"times"`
 }
 
 type Sessions struct {
-	ID				uint			`gorm:"primary key;autoIncrement" json:"id"`
-	Code			Code			`json:"code"`
+	ID						int			`gorm:"autoIncrement" json:"id"`
+	Coding				[]Code
 	// Workout		*string		`json:"workout"`
-	// Rest			*string		`json:"rest"`
+	// Rest				*string		`json:"rest"`
 }
 
 func MigrateSessions(db *gorm.DB) error {
-	err := db.AutoMigrate(&Sessions{})
+	err := db.AutoMigrate(&Sessions{}, &Code{}, &Timespan{})
 	return err
 }
